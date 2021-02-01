8d6737ef1d299b21203ee3ef989a43504e934a5b
Kristel Lamaj <kri.lamaj@gmail.com>
2021-02-01 22:11:07 +0200

    Insert Styles to Head

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,8 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>';
 
         // Styles
-        $header .= '<link rel=\'stylesheet\' href=\'./db_config/db_config.php\'>';
+        $header .= '<link rel=\'stylesheet\' href=\'./css/global_styles.css\'>';
+        $header .= '<link rel=\'stylesheet\' href=\'./css/styles.css\'>';
 
         // Scripts
         $header .= '';
@@ -24,7 +25,12 @@
         // Font Awesome 5.15.2
         $header .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />';
 
-        // Close tag
+        // Meta 
+        $header .= '<meta charset="utf-8">';
+        $header .= '<meta name="description" content="Huntsman Studio Website">';
+        $header .= '<meta name="author" content="Huntsman Studio Developers Department">';
+
+        // Close TAG
         $header .= '</head>';
 
         echo $header;
