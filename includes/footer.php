f4ce0c48ef934dc692a2775df323d891be64eafc
Kristel Lamaj <kri.lamaj@gmail.com>
2021-02-01 22:30:01 +0200

    Footer Functionality & Design

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<?php
+    // Footer Functionality
+    function get_footer(){
+
+        // Open DIV
+        $footer = '<div class="footer">';
+
+        // Close Div
+        $footer .= '</div>';
+
+        echo $footer;
+    }
+?>
