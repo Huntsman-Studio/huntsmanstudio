0e22928cdde20dd31b22d774ea7cb9e3cee0c0e4
Kristel Lamaj <kri.lamaj@gmail.com>
2021-09-22 22:57:30 +0300

    Components that will be used

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { MainComponent } from './main/main.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 // Set routes
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full'},
-  { path: 'nav', component: NavigationComponent }
+  { path: 'nav', component: NavigationComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { MenuComponent } from './menu/menu.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { NavigationComponent } from './navigation/navigation.component';
     MenuComponent,
     MainComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    WorkComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
