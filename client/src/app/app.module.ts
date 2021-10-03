a8af5598ca7d6e8125c5c9ddaa9725fe8d99bd1f
klamaj <kri.lamaj@gmail.com>
2021-10-03 23:59:10 +0300

    Update footer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { WorkComponent } from './work/work.component';
 import { AboutComponent } from './about/about.component';
@@ -16,7 +15,6 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     MenuComponent,
     MainComponent,
-    FooterComponent,
     NavigationComponent,
     WorkComponent,
     AboutComponent,
