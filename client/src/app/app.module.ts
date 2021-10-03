1b361d37e92f98e0f9828087b22f9ae071b71680
klamaj <kri.lamaj@gmail.com>
2021-10-04 00:16:57 +0300

    Install ngx-clipboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClipboardModule } from 'ngx-clipboard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClipboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
