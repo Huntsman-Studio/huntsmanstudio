c12c2ba2ef8cdb53f913d5f65887842f983998e0
klamaj <kri.lamaj@gmail.com>
2021-10-15 20:11:31 +0300

    Hide menu on nav

--- client/src/app/mobile-menu/mobile-menu.component.ts
+++ client/src/app/mobile-menu/mobile-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-menu',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _router: Router) { }
 
   ngOnInit(): void {
   }
