a7b9c23de66ee70f156fb1d75b0d26d69a6e0447
klamaj <kri.lamaj@gmail.com>
2022-03-21 21:36:21 +0200

    nguniversal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
     MobileMenuComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ClipboardModule,
   ],
