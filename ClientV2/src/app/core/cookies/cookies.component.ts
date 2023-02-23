96c0756228ecc2897075dea3f65d1265dc6a8a7d
Kristel Lamaj <kri.lamaj@gmail.com>
2023-02-23 23:48:47 +0200

    MB: Cookies bar

--- ClientV2/src/app/core/cookies/cookies.component.ts
+++ ClientV2/src/app/core/cookies/cookies.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cookies',
   templateUrl: './cookies.component.html',
   styleUrls: ['./cookies.component.scss']
 })
-export class CookiesComponent {
+export class CookiesComponent implements OnInit {
 
+  cookie: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleCookie(): void {
+    if (!this.cookie) {
+      this.cookie = true;
+    } else {
+      this.cookie = false;
+    }
+  }
 }
