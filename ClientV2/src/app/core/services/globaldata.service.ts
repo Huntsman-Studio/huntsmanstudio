04eea298a028e11e155c35d79084b9ce37f19edc
Kristel Lamaj <kri.lamaj@gmail.com>
2023-03-21 18:42:31 +0200

    complete responsive navigation

--- ClientV2/src/app/core/services/globaldata.service.ts
+++ ClientV2/src/app/core/services/globaldata.service.ts
@@ -7,11 +7,17 @@ import { BehaviorSubject } from 'rxjs';
 export class GlobaldataService {
 
   public menu: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public lang: BehaviorSubject<string> = new BehaviorSubject<string>("el");
 
   constructor() { }
 
   // showHideMenu
   showHideMenu(value: boolean) {
     this.menu.next(value);
+  }
+
+  // setLanguage
+  language(lang: string): void {
+    this.lang.next(lang);
   }
 }
