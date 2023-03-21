0d6b4a3fba3071fe0ae586542fa37a8b187a797b
Kristel Lamaj <kri.lamaj@gmail.com>
2023-03-21 18:49:13 +0200

    Add contact language functionality

--- ClientV2/src/app/modules/pages/contact/contact.component.ts
+++ ClientV2/src/app/modules/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { Clipboard } from '@angular/cdk/clipboard';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
@@ -9,7 +10,14 @@ import { Clipboard } from '@angular/cdk/clipboard';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(private meta: Meta, private title: Title, private clipboard: Clipboard) {}
+  constructor(private meta: Meta, 
+    private title: Title, 
+    private clipboard: Clipboard,
+    public translate: TranslateService) {
+      // set languages
+      translate.addLangs(['en', 'el', 'tr', 'it']);
+      translate.setDefaultLang('el');
+    }
 
   ngOnInit(): void {}
 
