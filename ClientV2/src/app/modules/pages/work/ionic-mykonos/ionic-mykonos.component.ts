2410e2f2b4f97d092ad5107260301baffe665693
Kristel Lamaj <kri.lamaj@gmail.com>
2023-03-26 14:31:15 +0300

    Complete presentation for IonicMykonos

--- ClientV2/src/app/modules/pages/work/ionic-mykonos/ionic-mykonos.component.ts
+++ ClientV2/src/app/modules/pages/work/ionic-mykonos/ionic-mykonos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-ionic-mykonos',
   templateUrl: './ionic-mykonos.component.html',
   styleUrls: ['./ionic-mykonos.component.scss']
 })
-export class IonicMykonosComponent {
+export class IonicMykonosComponent implements OnInit {
 
+  constructor(private meta: Meta,
+    private title: Title,
+    public translate: TranslateService) {
+      // set language
+      translate.addLangs(['en', 'el', 'tr', 'it']);
+      translate.setDefaultLang('el');
+    }
+
+  ngOnInit(): void {
+    
+  }
 }
