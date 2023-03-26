f6957e023601f4d39e5a560fd8992e9158801187
Kristel Lamaj <kri.lamaj@gmail.com>
2023-03-26 13:39:04 +0300

    Complete Kernel Presentation

--- ClientV2/src/app/modules/pages/work/kernel/kernel.component.ts
+++ ClientV2/src/app/modules/pages/work/kernel/kernel.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-kernel',
   templateUrl: './kernel.component.html',
   styleUrls: ['./kernel.component.scss']
 })
+
 export class KernelComponent implements OnInit {
 
-  constructor(private meta: Meta, private title: Title) {}
+  constructor(private meta: Meta, 
+    private title: Title,
+    public translate: TranslateService) {
+      // set language
+      translate.addLangs(['en', 'el', 'tr', 'it']);
+      translate.setDefaultLang('el');
+    }
 
   ngOnInit(): void {
-    
+
   }
 }
